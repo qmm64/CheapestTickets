e8207057ba57f514370b7c0e4448aeea5e9aef24
qmm64 <10gmagma01@gmail.com>
2025-10-25 23:17:04 +0700

    FlightRoute.cs перенесён в библиотеку

--- ConsoleApp1/Models/FlightRequest.cs
+++ ConsoleApp1/Models/FlightRequest.cs
@@ -1,9 +1,10 @@
-﻿
+﻿using CheapestTickets.Shared.Models;
+
 namespace CheapestTickets.Server.Models
 {
     internal class FlightRequest
     {
-        public List<FlightRoute> Routes { get; set; }
+        public List<Shared.Models.FlightRoute> Routes { get; set; }
         public int Days { get; set; }
         public bool IncludeAllPrices { get; set; } = false;
     }
