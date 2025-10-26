eaa4161e25616cf78034b92576012abe9eb05207
qmm64 <10gmagma01@gmail.com>
2025-10-26 15:32:18 +0700

    написан AppDbContext.cs

---
+++ ConsoleApp1/Database/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CheapestTickets.Server.Database
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<LogEntry> Logs => Set<LogEntry>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string connectionString = "Host=localhost;Port=5432;Database=cheapest_tickets;Username=agmin;Password=admin";
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LogEntry>(entity =>
+            {
+                entity.ToTable("logs");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Timestamp).IsRequired();
+                entity.Property(e => e.Source).HasMaxLength(100);
+                entity.Property(e => e.Ip).HasMaxLength(45);
+                entity.Property(e => e.Message).HasMaxLength(2000);
+            });
+        }
+    }
+}
