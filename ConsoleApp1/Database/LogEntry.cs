7c51dc26712be5906e54ab4ecdcbe046bddbff62
qmm64 <10gmagma01@gmail.com>
2025-10-26 15:27:24 +0700

    Создана модель логирования LogEntry.cs

---
+++ ConsoleApp1/Database/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CheapestTickets.Server.Database
+{
+    internal class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Source { get; set; } = string.Empty;
+        public string? Ip { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
